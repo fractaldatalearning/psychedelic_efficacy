76af9b7baaf9eb3ba89934fed08c91778ea0e16c
fractaldatalearning <104742545+fractaldatalearning@users.noreply.github.com>
2022-12-28 15:36:17 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='pHow might we predict and improve the efficacy of psychiatric treatment with a prescription psych med or psychedelic option, given demographic information about a patient and/or narrative description of their experiences before and after trying a drug?',
+    author='Katin Lind',
+    license='MIT',
+)
