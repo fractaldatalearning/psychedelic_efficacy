da9442265268989d2b855e348b0ec6df320a0617
fractaldatalearning <104742545+fractaldatalearning@users.noreply.github.com>
2022-12-31 18:55:12 -0800

    regex parsing underway

--- src/nlp/parse.py
+++ src/nlp/parse.py
@@ -1,4 +1,6 @@
 import unicodedata # for function remove_accented_chars
+from tqdm import tqdm # for pogress bar on a loop inside a function
+
 
 def remove_accented_chars(text):
     """Turns accented characters i.e. é, ä into unaccented characters ie e, a, in a string
@@ -12,3 +14,36 @@ def remove_accented_chars(text):
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
     return text
 
+
+
+def strip_most_punc(df, column):
+    """Deletes most common symbols from a string.
+    Does not delete the following strings ' : ; () ! ? # %
+    Comes with progress bar built in. 
+
+    Args: 
+      df: name of dataframe
+      column: name of a column from the dataframe 
+
+    Returns: same string in the same position within the df, with listed characters removed
+
+    """
+    punc = str.maketrans('', '', '-[]{}",<>./@^&*_~')
+    for row in tqdm(range(len(df))):
+        df.loc[row,column] = df.loc[row,column].translate(punc)
+
+
+
+def strip_apostrophe(df, column):
+    """Deletes apostrophes wherever they appear in strings in a dataframe column. 
+
+    Args: 
+      df: name of dataframe
+      column: name of a column from the dataframe 
+
+    Returns: same string in the same position within the df, with apostrophes removed
+
+    """
+    punc = str.maketrans('', '', "'")
+    for row in tqdm(range(len(df))):
+        df.loc[row,column] = df.loc[row,column].translate(punc)
--- tests/test_nlp/test_parse.py
+++ tests/test_nlp/test_parse.py
@@ -3,19 +3,37 @@ import pytest
 
 import pandas as pd
 
-from src.nlp.parse import remove_accented_chars
+from src.nlp.parse import remove_accented_chars, strip_most_punc, \
+     strip_apostrophe
 
 
 test_df = pd.DataFrame({'t_rating':[1,2,3,4,5], \
                         't_review':["I'd like some tea.", \
                                     "whére are yoü?", \
                                     "GIVE ME SOMETHING", \
-                                    "why!?!?!?!?", \
+                                    '-[]{}",<>./@^&*_~:;()!?#%', \
                                     "nower to be foun"]})
 
 def test_remove_accented_chars():
     assert test_df['t_review'].apply(remove_accented_chars)[1] == \
            'where are you?'
            
-           
 
+
+def test_strip_most_punc():
+    strip_most_punc(test_df, 't_review')
+    assert test_df.t_review[3] == ':;()!?#%'
+
+
+    
+def test_strip_apostrophe():
+    test_df = pd.DataFrame({'t_rating':[1,2,3,4,5], \
+                        't_review':["I'd like some tea.", \
+                                    "whére are yoü?", \
+                                    "GIVE ME SOMETHING", \
+                                    '-[]{}",<>./@^&*_~:;()!?#%', \
+                                    "nower to be foun"]})
+
+    strip_apostrophe(test_df, 't_review')
+    assert test_df.t_review[0] == "Id like some tea."
+    
